8268dd880b4b869fff01a4a950cc0a90563255ff
unknown <YY442@ccs.local>
2025-04-11 04:16:40 -0700

    assignment in accounts model, service and controller for rubi and de luna

---
+++ accounts/accounts.controller.js
@@ -0,0 +1,4 @@
+// de luna first
+// authenticate & authenticate shcema, register/schema, verify email/schema - rubi
+// refresh token, refresh token schema, revoke token schema,  forgot pass validate reset token schema, reset pass/schema,
+// get all, get by id, create shcema/create, update schema, delete schema, set token cookie- de luna
