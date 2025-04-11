58860a7aad526bba098ff729677804cbddd08b96
deLuna18 <alexahontiveros494@gmail.com>
2025-04-12 01:56:42 +0800

    added functions to accounts.controller.js

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -1,4 +1,208 @@
 // de luna first
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const authorize = require('_middleware/authorize');
+const Role = require('_helpers/role');
+const accountService = require('./account.service');
+
+// routes
+router.post('/authenticate', authenticateSchema, authenticate);
+router.post('/refresh-token', refreshToken);
+router.post('/revoke-token', authorize(), revokeTokenSchema, revokeToken);
+router.post('/register', registerSchema, register);
+router.post('/verify-email', verifyEmailSchema, verifyEmail);
+router.post('/forgot-password', forgotPasswordSchema, forgotPassword);
+router.post('/validate-reset-token', validateResetTokenSchema, validateResetToken);
+router.post('/reset-password', resetPasswordSchema, resetPassword);
+router.get('/', authorize(Role.Admin), getAll);
+router.get('/:id', authorize(), getById);
+router.post('/', authorize(Role.Admin), createSchema, create);
+router.put('/:id', authorize(), updateSchema, update);
+router.delete('/:id', authorize(), _delete);
+
+module.exports = router;
 // authenticate & authenticate shcema, register/schema, verify email/schema - rubi
-// refresh token, refresh token schema, revoke token schema,  forgot pass validate reset token schema, reset pass/schema,
+
+// refresh token, refresh token schema, revoke token schema,  forgot pass validate reset token schema, reset pass/schema- de luna
 // get all, get by id, create shcema/create, update schema, delete schema, set token cookie- de luna
+
+// authenticateSchema - rubi 
+
+// authenticate - rubi
+
+
+// refresh token - de luna
+function refreshToken(req, res, next) {
+    const token = req.cookies.refreshToken;
+    const ipAddress = req.ip;
+    accountService.refreshToken({ token, ipAddress })
+      .then(({ refreshToken, ...account }) => {
+        setTokenCookie(res, refreshToken);
+        res.json(account);
+      })
+      .catch(next);
+}
+// revoke token schema - de luna
+function revokeTokenSchema(req, res, next) {
+  const schema = Joi.object({
+    token: Joi.string().empty('')
+  });
+  validateRequest(req, next, schema);
+}
+// revoke token - de luna
+function revokeToken(req, res, next) {
+  // accept token from request body or cookie
+  const token = req.body.token || req.cookies.refreshToken;
+  const ipAddress = req.ip;
+
+  if (!token) return res.status(400).json({ message: 'Token is required' });
+
+  // users can revoke their own tokens and admins can revoke any tokens
+  if (!req.user.ownsToken(token) && req.user.role !== Role.Admin) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  accountService.revokeToken({ token, ipAddress })
+    .then(() => res.json({ message: 'Token revoked' }))
+    .catch(next);
+}
+
+// registerSchema - rubi
+
+// register - rubi
+
+// verify email schema - rubi
+
+// verify email - rubi
+
+// Forgot Password Schema - de luna
+function forgotPasswordSchema(req, res, next) {
+  const schema = Joi.object({
+    email: Joi.string().email().required()
+  });
+  validateRequest(req, next, schema);
+}
+// Forgot Password - de luna
+function forgotPassword(req, res, next) {
+  accountService.forgotPassword(req.body, req.get('origin'))
+    .then(() => res.json({ message: 'Please check your email for password reset instructions' }))
+    .catch(next);
+}
+// Validate Reset Token Schema - de luna
+function validateResetTokenSchema(req, res, next) {
+  const schema = Joi.object({
+    token: Joi.string().required()
+  });
+  validateRequest(req, next, schema);
+}
+// Validate Reset Token - de luna
+function validateResetToken(req, res, next) {
+  accountService.validateResetToken(req.body)
+    .then(() => res.json({ message: 'Token is valid' }))
+    .catch(next);
+}
+// resert password schema - de luna
+function resetPasswordSchema(req, res, next) {
+  const schema = Joi.object({
+    token: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+  });
+  validateRequest(req, next, schema);
+}
+// resert password - de luna
+function resetPassword(req, res, next) {
+  accountService.resetPassword(req.body)
+    .then(() => res.json({ message: 'Password reset successful, you can now login' }))
+    .catch(next);
+}
+// get all - de luna
+function getAll(req, res, next) {
+  accountService.getAll()
+    .then(accounts => res.json(accounts))
+    .catch(next);
+}
+// get by id - de luna
+function getById(req, res, next) {
+  // users can get their own account and admins can get any account
+  if (Number(req.params.id) !== req.user.id && req.user.role !== Role.Admin) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  accountService.getById(req.params.id)
+    .then(account => account ? res.json(account) : res.sendStatus(404))
+    .catch(next);
+}
+// create schema - de luna
+function createSchema(req, res, next) {
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
+    role: Joi.string().valid(Role.Admin, Role.User).required()
+  });
+  validateRequest(req, next, schema);
+}
+// create - de luna
+function create(req, res, next) {
+  accountService.create(req.body)
+    .then(account => res.json(account))
+    .catch(next);
+}
+// updateSchema - de luna   
+function updateSchema(req, res, next) {
+  const schemaRules = {
+    title: Joi.string().empty(''),
+    firstName: Joi.string().empty(''),
+    lastName: Joi.string().empty(''),
+    email: Joi.string().email().empty(''),
+    password: Joi.string().min(6).empty(''),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
+  };
+
+  // only admins can update role
+  if (req.user.role === Role.Admin) {
+    schemaRules.role = Joi.string().valid(Role.Admin, Role.User).empty('');
+  }
+
+  const schema = Joi.object(schemaRules).with('password', 'confirmPassword');
+  validateRequest(req, next, schema);
+}
+// Update - de luna
+function update(req, res, next) {
+  // users can update their own account and admins can update any account
+  if (Number(req.params.id) !== req.user.id && req.user.role !== Role.Admin) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  accountService.update(req.params.id, req.body)
+    .then(account => res.json(account))
+    .catch(next);
+}
+// delete - deluna 
+function _delete(req, res, next) {
+    // users can delete their own account and admins can delete any account
+    if (Number(req.params.id) !== req.user.id && req.user.role !== Role.Admin) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+  
+    accountService.delete(req.params.id)
+      .then(() => res.json({ message: 'Account deleted successfully' }))
+      .catch(next);
+}
+  
+// helper functions
+// set token cookie - de luna
+function setTokenCookie(res, token) {
+    // create cookie with refresh token that expires in 7 days
+    const cookieOptions = {
+      httpOnly: true,
+      expires: new Date(Date.now() + 7*24*60*60*1000)
+    };
+    res.cookie('refreshToken', token, cookieOptions);
+}  
