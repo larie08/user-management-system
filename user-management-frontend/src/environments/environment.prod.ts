6ea82d5d3114409f75338c3a4134d5dadcc8af5d
Cedric E. Cornelio <cedcedcornelio123@gmail.com>
2025-05-19 02:59:17 +0800

    Deploying Frontend

--- user-management-frontend/src/environments/environment.prod.ts
+++ user-management-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://user-management-system-backend-2lhk.onrender.com'
+  apiUrl: 'https://user-management-system-backend-2lhk.onrender.com'
 };
