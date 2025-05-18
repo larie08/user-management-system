0b0e545e91e2cc7c7f5069107c71b5b80ae931ce
Cedric E. Cornelio <cedcedcornelio123@gmail.com>
2025-05-19 01:28:04 +0800

    Trying to connect to db

--- user-management-backend/server.js
+++ user-management-backend/server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('_middleware/error-handler');
-const config = require('./config');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
